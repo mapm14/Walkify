a078dd2da461f08b605ee51fb3ab07f6fec3eddc
Manuel Perera <mapm14@gmail.com>
2020-03-18 21:13:38 +0100

    Changes in BaseViewMovel

--- presentation/src/main/java/manuelperera/walkify/presentation/ui/base/viewmodel/BaseViewModel.kt
+++ presentation/src/main/java/manuelperera/walkify/presentation/ui/base/viewmodel/BaseViewModel.kt
@@ -1,10 +1,15 @@
 package manuelperera.walkify.presentation.ui.base.viewmodel
 
 import android.content.res.Resources
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.chuckerteam.chucker.api.ChuckerCollector
 import dagger.Lazy
 import io.reactivex.disposables.CompositeDisposable
+import manuelperera.walkify.domain.entity.base.Failure
+import manuelperera.walkify.presentation.BuildConfig
+import manuelperera.walkify.presentation.R
 import javax.inject.Inject
 
 abstract class BaseViewModel : ViewModel() {
@@ -15,6 +20,12 @@ abstract class BaseViewModel : ViewModel() {
     @Inject
     lateinit var chuckerCollector: Lazy<ChuckerCollector>
 
+    private val _ldLoading: MutableLiveData<Unit> = MutableLiveData()
+    val ldLoading: LiveData<Unit> = _ldLoading
+
+    private val _ldFailure: MutableLiveData<Failure> = MutableLiveData()
+    val ldFailure: LiveData<Failure> = _ldFailure
+
     protected val compositeDisposable = CompositeDisposable()
 
     override fun onCleared() {
@@ -22,8 +33,24 @@ abstract class BaseViewModel : ViewModel() {
         super.onCleared()
     }
 
-    protected fun recordError(throwable: Throwable) {
+    protected fun loading() {
+        _ldLoading.value = Unit
+    }
+
+    protected fun handleFailure(throwable: Throwable, retryAction: () -> Unit) {
         chuckerCollector.get().onError(throwable::class.java.simpleName, throwable)
+
+        val failure = throwable as? Failure ?: kotlin.run {
+            val message = if (BuildConfig.BUILD_TYPE == "release" || throwable.message?.isEmpty() == true) {
+                resources.get().getString(R.string.unknown_error)
+            } else {
+                throwable.message ?: resources.get().getString(R.string.unknown_error)
+            }
+            Failure.Error(message)
+        }
+        failure.retryAction = retryAction
+
+        _ldFailure.value = failure
     }
 
 }
