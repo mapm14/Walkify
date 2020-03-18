ef115f571457912f1fef55547463a7527f59801b
Manuel Perera <mapm14@gmail.com>
2020-03-18 19:52:42 +0100

    Create Glide Module

---
+++ presentation/src/main/java/manuelperera/walkify/presentation/ui/base/MyAppGlideModule.kt
@@ -0,0 +1,7 @@
+package manuelperera.walkify.presentation.ui.base
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+@GlideModule
+class MyAppGlideModule : AppGlideModule()
