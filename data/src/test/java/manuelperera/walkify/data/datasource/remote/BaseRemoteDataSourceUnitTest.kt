5005a7f74411bed15e8637e5d7d1a137528b00a2
Manuel Perera <mapm14@gmail.com>
2020-03-21 23:54:50 +0100

    Add TODO for BaseRemoteDataSource tests

---
+++ data/src/test/java/manuelperera/walkify/data/datasource/remote/BaseRemoteDataSourceUnitTest.kt
@@ -0,0 +1,15 @@
+package manuelperera.walkify.data.datasource.remote
+
+import org.junit.Before
+import org.junit.runner.RunWith
+import org.mockito.junit.MockitoJUnitRunner
+
+// TODO: Missing tests
+@RunWith(MockitoJUnitRunner::class)
+class BaseRemoteDataSourceUnitTest {
+
+    @Before
+    fun setUp() {
+    }
+
+}
